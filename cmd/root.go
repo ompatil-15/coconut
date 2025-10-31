43fc8cb488d59267c27137c899a70145d58b43c2
Om Patil <patilom001@gmail.com>
2025-10-31 22:33:34 +0530

    feat: coconut password manager init

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "coconut",
+	Short: "A CLI password manager with Zero Knowledge Architecture",
+	Long: `coconut is a CLI password manager with Zero Knowledge Architecture
+
+Store all your passwords effortlessly while having to only remember a
+single master password. With the Zero Knowledge Architecture, your 
+passwords are safe even after full device compromise.
+`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+// Author: Om Patil <patilom001@gmail.com>
+package main
+
+import "github.com/ompatil-15/coconut/cmd"
+
+func main() {
+	cmd.Execute()
+}
